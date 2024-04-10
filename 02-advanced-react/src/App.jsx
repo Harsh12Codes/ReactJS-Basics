61de9e611856664bc3b8cb1f87b39309588589cb
Harsh Yadav <harsh12codes@gmail.com>
2024-04-10 19:06:23 +0530

    fix

--- 02-advanced-react/src/App.jsx
+++ 02-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
+import Starter from "./tutorial/06-forms/starter/02-user-challenge";
 
 function App() {
-  return (
-    <div className="container">
-      <Starter />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Starter />
+        </div>
+    );
 }
 
 export default App;
